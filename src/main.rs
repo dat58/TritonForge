c4a4558f45d9cf44d584af2e4e48fa1c001bc73f
dat58 <vtdat58@gmail.com>
2026-05-08 03:01:59 +0000

    refactor(logging): Simplify and streamline tracing subscriber initialization

--- src/main.rs
+++ src/main.rs
@@ -40,13 +40,6 @@ fn init_tracing() {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::from_default_env())
-        .with(
-            tracing_subscriber::fmt::layer()
-                .json()
-                .with_target(true)
-                .with_thread_ids(true)
-                .with_file(true)
-                .with_line_number(true),
-        )
+        .with(tracing_subscriber::fmt::layer().with_target(true))
         .init();
 }
